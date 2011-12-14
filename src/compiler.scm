230ef0107817cc43afe4a5e73bab648b442299ec
Nada Amin <namin@alum.mit.edu>
2011-12-15 00:59:34 +0100

    1.2 Immediate Constants

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,5 +1,36 @@
 (load "tests-driver.scm")
+(load "tests-1.2-req.scm")
 (load "tests-1.1-req.scm")
+
+(define fxshift        2)
+(define fxmask      #x03)
+(define bool-f      #x2F)
+(define bool-t      #x6F)
+(define list-nil    #x3F)
+(define charshift      8)
+(define charmask    #x0F)
+(define chartag     #x0F)
+(define wordsize       4) ; bytes
+
+(define fixnum-bits (- (* wordsize 8) fxshift))
+
+(define fxlower (- (expt 2 (- fixnum-bits 1))))
+
+(define fxupper (sub1 (expt 2 (- fixnum-bits 1))))
+
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (null? x) (char? x)))
+
+(define (immediate-rep x)
+  (cond
+   [(fixnum? x) (ash x fxshift)]
+   [(boolean? x) (if x bool-t bool-f)]
+   [(null? x) list-nil]
+   [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
+   [else #f]))
 
 (define (emit-function-header f)
   (emit "  .text")
@@ -8,7 +39,7 @@
   (emit "~a:" f))
 
 (define (emit-program x)
-  (unless (integer? x) (error 'compile-program "not an integer"))
+  (unless (immediate? x) (error 'emit-program (format "'~s' not an immediate" x)))
   (emit-function-header "scheme_entry")
-  (emit "  movl $~s, %eax" x)
+  (emit "  movl $~s, %eax" (immediate-rep x))
   (emit "  ret"))
--- src/startup.c
+++ src/startup.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 
-int scheme_entry();
+/* define all scheme constants */
+#define bool_f               0x2F
+#define bool_t               0x6F
+#define list_nil             0x3F
+#define fx_mask              0x03
+#define fx_tag               0x00
+#define fx_shift                2
+#define char_mask            0x0F
+#define char_tag             0x0F
+#define char_shift              8
+
+
+/* all scheme values are of type ptrs */
+typedef unsigned int ptr;
+
+ptr scheme_entry();
+
+static void print_ptr(ptr x) {
+  if ((x & fx_mask) == fx_tag) {
+    printf("%d", ((int) x) >> fx_shift);
+  } else if (x == bool_f) {
+    printf("#f");
+  } else if (x == bool_t) {
+    printf("#t");
+  } else if (x == list_nil) {
+    printf("()");
+  } else if ((x & char_mask) == char_tag) {
+    char c = (char) (x >> char_shift);
+    if      (c == '\t') printf("#\\tab");
+    else if (c == '\n') printf("#\\newline");
+    else if (c == '\r') printf("#\\return");
+    else if (c == ' ')  printf("#\\space");
+    else                printf("#\\%c", c);
+  } else {
+    printf("#<unknown 0x%08x>", x);
+  }
+  printf("\n");
+}
 
 int main(int argc, char** argv) {
-  printf("%d\n", scheme_entry());
+  print_ptr(scheme_entry());
   return 0;
 }
