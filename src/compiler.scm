bce844e31c64ff234baabe4cf2e220d2a9713337
Nada Amin <namin@alum.mit.edu>
2011-12-15 02:14:43 +0100

    1.3 Unary Primitives

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,14 +1,18 @@
 (load "tests-driver.scm")
+(load "tests-1.3-req.scm")
 (load "tests-1.2-req.scm")
 (load "tests-1.1-req.scm")
 
 (define fxshift        2)
 (define fxmask      #x03)
+(define fxtag       #x00)
 (define bool-f      #x2F)
 (define bool-t      #x6F)
+(define bool-bit       6)
+(define boolmask    #xBF)
 (define list-nil    #x3F)
 (define charshift      8)
-(define charmask    #x0F)
+(define charmask    #x3F)
 (define chartag     #x0F)
 (define wordsize       4) ; bytes
 
@@ -32,6 +36,104 @@
    [(char? x) (bitwise-ior (ash (char->integer x) charshift) chartag)]
    [else #f]))
 
+(define (emit-immediate x)
+  (emit "  movl $~s, %eax" (immediate-rep x)))
+
+(define-syntax define-primitive
+  (syntax-rules ()
+    [(_ (prim-name arg* ...) b b* ...)
+     (begin
+       (putprop 'prim-name '*is-prim* #t)
+       (putprop 'prim-name '*arg-count*
+         (length '(arg* ...)))
+       (putprop 'prim-name '*emitter*
+         (lambda (arg* ...) b b* ...)))]))
+
+(define (primitive? x)
+  (and (symbol? x) (getprop x '*is-prim*)))
+
+(define (primitive-emitter x)
+  (or (getprop x '*emitter*) (error 'primitive-emitter (format "primitive ~s has no emitter" x))))
+
+(define (primcall? expr)
+  (and (pair? expr) (primitive? (car expr))))
+
+(define (check-primcall-args prim args)
+  (= (getprop prim '*arg-count*) (length args)))
+
+(define (emit-primcall expr)
+  (let ([prim (car expr)] [args (cdr expr)])
+    (check-primcall-args prim args)
+    (apply (primitive-emitter prim) args)))
+
+(define-primitive (fxadd1 arg)
+  (emit-expr arg)
+  (emit "  addl $~s, %eax" (immediate-rep 1)))
+
+(define-primitive (fxsub1 arg)
+  (emit-expr arg)
+  (emit "  subl $~s, %eax" (immediate-rep 1)))
+
+(define-primitive (fixnum->char arg)
+  (emit-expr arg)
+  (emit "  shll $~s, %eax" (- charshift fxshift))
+  (emit "  orl $~s, %eax" chartag))
+
+(define-primitive (char->fixnum arg)
+  (emit-expr arg)
+  (emit "  shrl $~s, %eax" (- charshift fxshift)))
+
+(define-primitive (fixnum? arg)
+  (emit-expr arg)
+  (emit "  and $~s, %al" fxmask)
+  (emit "  cmp $~s, %al" fxtag)
+  (emit-cmp-bool))
+
+(define (emit-cmp-bool)
+  (emit "  sete %al")
+  (emit "  movzbl %al, %eax")
+  (emit "  sal $~s, %al" bool-bit)
+  (emit "  or $~s, %al" bool-f))
+
+(define-primitive (fxzero? arg)
+  (emit-expr arg)
+  (emit "  cmpl $~s, %eax" fxtag)
+  (emit-cmp-bool))
+
+(define-primitive (null? arg)
+  (emit-expr arg)
+  (emit "  cmp $~s, %al" list-nil)
+  (emit-cmp-bool))
+
+(define-primitive (boolean? arg)
+  (emit-expr arg)
+  (emit "  and $~s, %al" boolmask)
+  (emit "  cmp $~s, %al" bool-f)
+  (emit-cmp-bool))
+
+(define-primitive (char? arg)
+  (emit-expr arg)
+  (emit "  and $~s, %al" charmask)
+  (emit "  cmp $~s, %al" chartag)
+  (emit-cmp-bool))
+
+(define-primitive (not arg)
+  (emit-expr arg)
+  (emit "  cmp $~s, %al" bool-f)
+  (emit-cmp-bool))
+
+(define-primitive (fxlognot arg)
+  (emit-expr arg)
+  (emit "  shr $~s, %eax" fxshift)
+  (emit "  not %eax")
+  (emit "  shl $~s, %eax" fxshift))
+
+(define (emit-expr expr)
+  (cond
+   [(immediate? expr) (emit-immediate expr)]
+   [(primcall? expr) (emit-primcall expr)]
+   [else (error 'emit-expr (format "~s is not an expression" expr))]))
+
 (define (emit-function-header f)
   (emit "  .text")
   (emit "  .globl ~a" f)
@@ -39,7 +141,6 @@
   (emit "~a:" f))
 
 (define (emit-program x)
-  (unless (immediate? x) (error 'emit-program (format "'~s' not an immediate" x)))
   (emit-function-header "scheme_entry")
-  (emit "  movl $~s, %eax" (immediate-rep x))
+  (emit-expr x)
   (emit "  ret"))
