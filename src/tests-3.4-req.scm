0dd674a11a6431d47121f7595898038e207b1811
Nada Amin <namin@alum.mit.edu>
2011-12-30 08:54:38 -0500

    3.4 Apply

--- src/tests-3.4-req.scm
+++ src/tests-3.4-req.scm
@@ -1,4 +1,3 @@
-
 (add-tests-with-string-output "nontail apply"
  [(let ([f (lambda () 12)])
    (fx+ (apply f '()) 1)) => "13\n"]
@@ -38,9 +37,6 @@
  [(apply vector 1 2 3 4 5 6 7 '(8)) => "#(1 2 3 4 5 6 7 8)\n"]
  [(apply vector 1 2 3 4 5 6 7 8 ()) => "#(1 2 3 4 5 6 7 8)\n"]
 )
-
-
-
 
 (add-tests-with-string-output "nontail apply"
  [(let ([f (lambda () 12)])
