94dd2d2acb578fb5514957585668edac0920ff3d
Nada Amin <namin@alum.mit.edu>
2012-01-08 21:41:44 -0500

    5.2 Garbage collection
    A wasteful breath-first copying algorithm. Inspired by Cheney's algorithm as described in section 16.2 of Appel's Compiling with Continuations. But less elegant and simple, as I need a separate queue for the scan in order to know the type of the objects in to-space still to be processed.
    
    Need to investigate why some tests can still not be bumped up to their pre-CPS-conversion efficiency.

--- src/ctest.c
+++ src/ctest.c
@@ -17,7 +17,7 @@ char fancy(char c) {
 }
 
 ptr scheme_entry(context* ctxt, char* stack_base, memory* mem) {
-  char* heap = mem->heap_cur;
+  char* heap = mem->heap_next;
   if (global_fun == 0) {
     global_fun = ((ptr) &inc) | closure_tag;
   }
