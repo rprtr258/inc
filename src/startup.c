83c7729f7623a4018f7772e9009b7ae6494a25fe
Nada Amin <namin@alum.mit.edu>
2012-01-13 23:39:52 +0100

    Remove extra skip after return address marker on stack.

--- src/startup.c
+++ src/startup.c
@@ -300,7 +300,6 @@ static void gc(memory* mem, char* stack) {
   while (root >= (ptr*) stack) {
     if (*root == return_addr) {
       root--; // skip return address
-      root--;
     } else {
       *root = gc_forward(*root);
     }
